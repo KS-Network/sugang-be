494edf778c60398caa4244ddfe1e4e4e92c5c1f6
haeramkeem <ewqdsacxz2345@gmail.com>
2022-11-21 22:53:42 +0900

    backup: sql queries

---
+++ db/lecture.sql
@@ -0,0 +1,21 @@
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('학과공통',1,3,'성인지교육1','1000-9581','00','정혜원',0,120);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('학과공통',1,3,'성인지교육2','1000-9582','00','정혜원',0,120);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('학과공통',1,3,'페미니즘과 고전','1001-7032','00','허현',0,30);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('학과공통',1,3,'법여성학','1005-7002','00','주현경',0,20);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('학과공통',1,3,'중국고대철학','5911-9797','00','남용우',0,40);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('교육학과',2,3,'인권과인성교육','1344-7003','00','박병휘',0,35);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('교육학과',2,3,'요가','1345-7001','00','김동화',0,25);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('정치외교학과',3,3,'분단과 통일','1136-1004','00','김해람',0,500);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('정치외교학과',3,3,'정치사상2','1136-2004','00','이욱근',0,40);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('정치외교학과',3,3,'북한의정치와문화','1136-2010','00','신진',0,40);
+
+INSERT INTO lecture (department,grade,credit,title,lecture_id,class_no,professor,quota,attendance) VALUES ('사회복지학과',3,3,'빈곤론','1135-3001','00','신진',0,40);
---
+++ db/student.sql
@@ -0,0 +1,9 @@
+INSERT INTO student (email,student_id,password,name,grade,credit) VALUES ('taenyeon.kim@gmail.com','ty.kim','tyk1234','김태년',1,30);
+
+INSERT INTO student (email,student_id,password,name,grade,credit) VALUES ('seokyeol.yun@gmail.com','sy.yun','syy1234','윤석열',2,40);
+
+INSERT INTO student (email,student_id,password,name,grade,credit) VALUES ('honggeun.park@gmail.com','hg.park','hgp1234','박홍근',3,50);
+
+INSERT INTO student (email,student_id,password,name,grade,credit) VALUES ('jaemyeong.lee@gmail.com','jm.lee','jml1234','이재명',3,60);
+
+INSERT INTO student (email,student_id,password,name,grade,credit) VALUES ('yongjin.park@gmail.com','yj.park','yjp1234','박용진',4,120);
