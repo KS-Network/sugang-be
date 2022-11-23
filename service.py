288982d2f45c26980c13816e77ddd05de254b8c9
oponeuser7 <oponeuser7@gmail.com>
2022-11-23 15:56:46 +0900

    feat: LectureNotFoundError - 2

--- service.py
+++ service.py
@@ -6,6 +6,9 @@ import exceptions
 def verify():
     return model.verify()
 
+def verify_admin():
+    return model.verify_admin();
+
 def sign_in(student: model.Student):
     data = model.sign_in(student)
     return data
@@ -23,22 +26,24 @@ def get_lecture(department: str, grade: str, professor: str, title: str, lecture
     return data
 
 def put_lecture(lecture: model.Lecture):
-    if not model.verify_admin(): raise exceptions.VerificationError('admin')
+    if not verify_admin(): raise exceptions.VerificationError('admin')
+    if not model.lecture_exists(lecture.lecture_id): raise exceptions.LectureNotFoundError(lecture.lecture_id)
     data = model.put_lecture(lecture)
     return data
 
 def delete_lecture(lecture_id: str):
-    if not model.verify_admin(): raise exceptions.VerificationError('admin')
+    if not verify_admin(): raise exceptions.VerificationError('admin')
+    if not model.lecture_exists(lecture_id): raise exceptions.LectureNotFoundError(lecture_id)
     data = model.delete_lecture(lecture_id)
     return data
 
 def get_student_lecture(student_id: str):
-    if not model.verify(): raise exceptions.VerificationError('student')
+    if not verify(): raise exceptions.VerificationError('student')
     data = model.get_student_lecture(student_id)
     return data
 
 def post_attendance(attendance: model.Attendance):
-    if not model.verify(): raise exceptions.VerificationError('student')
+    if not verify(): raise exceptions.VerificationError('student')
     time = model.get_time()
     if not model.check_time(): raise exceptions.RequestTimeError(time['start'], time['end'])
     if not model.lecture_exists(attendance.lecture_id): raise exceptions.LectureNotFoundError(attendance.lecture_id)
@@ -47,12 +52,12 @@ def post_attendance(attendance: model.Attendance):
     return data
 
 def delete_attendance(lecture_id: str, student_id: str):
-    if not model.verify(): raise exceptions.VerificationError('student')
+    if not verify(): raise exceptions.VerificationError('student')
     data = model.delete_attendance(lecture_id, student_id)
     return data
 
 def put_time(time):
-    if not model.verify_admin(): raise exceptions.VerificationError('admin')
+    if not verify_admin(): raise exceptions.VerificationError('admin')
     data = model.put_time(time)
     return data
 
